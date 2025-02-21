262c2181a148a582f54aaa6a896a35b2088c36b5
Maria Rosales <maria99r@hotmail.com>
2025-02-21 19:01:22 +0100

    actividades terminado

--- app/src/main/java/com/alanturing/nebula/model/activities/ActivitiesClient.kt
+++ app/src/main/java/com/alanturing/nebula/model/activities/ActivitiesClient.kt
@@ -1,15 +1,10 @@
 package com.alanturing.nebula.model.activities
 
-import com.alanturing.nebula.model.authentication.AuthRequest
-import com.alanturing.nebula.model.authentication.LoginResponse
-import com.alanturing.nebula.model.authentication.SignUpResponse
-import com.alanturing.nebula.model.authentication.TokenRequest
-import com.alanturing.nebula.model.authentication.TokenResponse
+
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
-import retrofit2.http.HTTP
 import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Path
@@ -17,12 +12,6 @@ import retrofit2.http.Path
 interface ActivitiesClient {
 
     // ACTIVIDADES ---------------------
-
-    @POST("/api/activity")
-    suspend fun createActivity(
-        @Header("Authorization") bearerToken: String,
-        @Body activityRequest: ActivityRequest
-    ) : Response<ActivityResponse>
 
     @GET("/api/activity")
     suspend fun getAllActivities(@Header("Authorization") bearerToken: String) : Response<List<ActivityResponse>>
@@ -36,8 +25,9 @@ interface ActivitiesClient {
     // PARTICIPACIONES --------------------
 
     @POST("/api/participation")
-    suspend fun addParticipation(@Header("Authorization") bearerToken: String,
-                                 @Body participationRequest: ParticipationRequest
+    suspend fun addParticipation(
+        @Header("Authorization") bearerToken: String,
+        @Body participationRequest: ParticipationRequest
     ) : Response<ParticipationResponse>
 
     @DELETE("/api/participation/{activityId}/{userId}")
